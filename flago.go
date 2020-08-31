47824bef8403d13fa1ce40a9a1a1f612a90a754f
hen6003 <henrysmith6003@gmail.com>
2020-08-31 15:53:39 +0100

    added nonflags and descriptons

--- flago.go
+++ flago.go
@@ -5,7 +5,8 @@ import (
 	"os"
 )
 
-func Exists(list []string) ([]bool, error) {
+// CheckFlags checks the flags against what flags you want
+func CheckFlags(list []string) ([]bool, error) {
 	args := os.Args[1:]
 
 	info := make([]bool, len(list))
@@ -14,7 +15,8 @@ func Exists(list []string) ([]bool, error) {
 		for i, s := range list {
 			switch len(s) {
 			case 0:
-				return info, errors.New("0 length string")
+				err := errors.New("0 width string")
+				return info, err
 
 			case 1:
 				s = "-" + s
@@ -30,4 +32,20 @@ func Exists(list []string) ([]bool, error) {
 	}
 
 	return info, nil
+}
+
+// NonFlags returns all args that are not flags
+func NonFlags() []string {
+	args := os.Args[1:]
+	var nonFlags []string
+
+	for _, v := range args {
+		if string(v[0]) != "-" {
+			nonFlags = append(nonFlags, v)
+		} else if v == "-" {
+			nonFlags = append(nonFlags, v)
+		}
+	}
+
+	return nonFlags
 }
